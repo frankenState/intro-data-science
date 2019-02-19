c132db6fb70b37da0727fe266775ca60f93df190
frankenState <ubayfranklou@gmail.com>
2019-02-20 01:49:50 +0800

    Twitter Sentiment Analysis - Learn Python for Data Science #2

--- video-2/sentiment_tweet.py
+++ video-2/sentiment_tweet.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'SoW6ymh73XmtFbYLspzH0AKpT'
-consumer_secret = 'Y6jyFdFcblahtYsY1OuUHLHgrpdmBPU3g1H64ApYPmSu0ZScZ5'
+consumer_key = 'CONSUMER_KEY'
+consumer_secret = 'CONSUMER_SECRET'
 
-access_token = '1097895123524890624-Sq2emXwCXY4HXBjz2C1LrC3H32DJ9B'
-access_token_secret = 'mr1eLViDnGvt7ByCaAKUzEMbaQE7JVIh0YiDoAH4pCuBr'
+access_token = 'ACCESS_TOKEN'
+access_token_secret = 'ACCESS_TOKEN_SECRET'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
